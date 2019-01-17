8023274549db5146882010df7a43df75a6ea4df8
Abhijeet Dubey <dubey.abhijeet09@gmail.com>
2019-01-17 16:56:52 +0530

    vocab.py: Add class for vocabulary

---
+++ src/vocab.py
@@ -0,0 +1,48 @@
+PAD_token = 0
+SOS_token = 1
+EOS_token = 2
+
+class Voc:
+    def __init__(self, name):
+        self.name = name
+        self.trimmed = False
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {PAD_token : "PAD", SOS_token : "SOS", EOS_token : "EOS"}
+        self.num_words = 3 # Count PAD, SOS, EOS
+
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.num_words
+            self.word2count[word] = 1
+            self.index2word[self.num_words] = word
+            self.num_words += 1
+        else:
+            self.word2count[word] += 1
+
+    # Remove words below a certain count threshold
+    def trim(self, min_count):
+        if self.trimmed:
+            return
+        self.trimmed = True
+        
+        keep_words = []
+        
+        for k, v in self.word2count.items():
+            if v >= min_count:
+                keep_words.append(k)
+
+        print('keep_words {}/{} = {:.4f}'.format(len(keep_words), len(word2index), len(keep_words)/len(word2index)))
+
+        # Reinitialize dictionaries
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {PAD_token : "PAD", SOS_token : "SOS", EOS_token : "EOS"}
+        self.num_words = 3
+
+        for word in keep_words:
+            self.addWord(word)
